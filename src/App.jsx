a663a9b09aab2798273a33ec503c8b03113eed7b
PKief <philipp.kief@gmx.de>
2016-09-06 14:48:01 +0200

    added more sample files with react code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Navigation from './Navigation';
 import Home from './Home';
 
+import styles from './styles.scss';
+
 export default React.createClass({
   render() {
     return <div>
-        <Navigation></Navigation>        
-        {this.props.children || <Home/>}
+        <Navigation></Navigation>
+        <div className={styles.routerOutlet}>
+          {this.props.children || <Home/>}
+        </div>                
     </div>
   }
 })
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -6,15 +6,16 @@ import styles from './Navigation.scss';
 export default React.createClass({
     render() {
         return <div>
-            <ul role="nav">
-                <li><NavLink to="/">Start (always active)</NavLink></li>
-                <li><NavLink to="/" onlyActiveOnIndex={true}>Start (onlyActiveOnIndex)</NavLink></li>                                
-                <li><IndexLink to="/" activeClassName={styles.active}>Start (indexlink)</IndexLink>                
+            <ul role="nav" className={styles.navigation}>
+                <li><NavLink to="/">Start (always active) </NavLink>
                     <ul>
                         <li><NavLink to="/about">About</NavLink></li>
+                        <li><NavLink to="/search">Search</NavLink></li>
                         <li><NavLink to="/repos">Repos</NavLink></li>
                     </ul>
                 </li>
+                <li><NavLink to="/" onlyActiveOnIndex={true}>Start (onlyActiveOnIndex) </NavLink></li>
+                <li><IndexLink to="/" activeClassName={styles.active}>Start (indexlink) </IndexLink></li>
                 <li><NavLink to="/counter">Counter</NavLink></li>
             </ul>
         </div>
