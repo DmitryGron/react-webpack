2dd3b5f5e71af68d2695f3a89088ac1d51b61475
PKief <philipp.kief@gmx.de>
2016-09-26 13:54:14 +0200

    added decorators

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,14 +2,43 @@ import React from 'react';
 
 export default class Home extends React.Component {
 
+    componentDidMount() {
+        let cat = new Cat('yo');
+        console.log(cat.name);
+        cat.meow = function(){console.log('hohoh')};
+        console.log(Cat.isSupercat);        
+    }
+
     render() {
         return (
             <div>
                 <div>Welcome Home</div>
-                {Math.pow(5,2)}   
+                {Math.pow(5, 2) }
                 <hr/>
-                            
+
             </div>
         )
+    }
+}
+
+// some decorators
+function supercat(target) {
+    target.isSupercat = true;
+    target.power = 'flight';
+}
+
+function readonly(target, key, descriptor) {
+    descriptor.writable = false;
+    return descriptor;
+}
+
+@supercat
+class Cat {
+    constructor(name){
+        this.name = name;
+    }
+    @readonly
+    meow(){
+        return 'yo';
     }
 }
